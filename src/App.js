3d399d27f47cecdb55e799a9d4d835e6646203da
Jason Noyes <jrnoyes1@gmail.com>
2022-09-12 20:28:01 -0500

    finished assignment

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <Counter />
-      <Todo />
+      <Todo/>
     </div>
   );
 }
--- src/features/todoSlice.js
+++ src/features/todoSlice.js
@@ -4,6 +4,8 @@ const initialState = {
     items: []
 }
 
+
+
 export const todoSlice = createSlice({
     name: 'todos',
     initialState,
@@ -12,7 +14,6 @@ export const todoSlice = createSlice({
             return { items: [...state.items, action.payload]}
         },
         removeOne: (state, action) => {
-            console.log(action)
             let array = [...state.items]
             let index = action.payload
             if(index !== -1) {
